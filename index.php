570ca55be1b4f32d347ac920806d8bd94c35ec28
Kevin Sanchez <kevinsb-93@hotmail.com>
2018-06-02 01:13:48 +0200

    COMMENT 4

--- index.php
+++ index.php
@@ -46,13 +46,12 @@ include_once 'dbconnect.php';
 
       <!--Componente principal de un mensaje de primario o llamado a la acción -->
       <div class="jumbotron" style="height: 864px;">
-          <div class="media">
-              <img class="align-self-start mr-3" src=".../64x64" alt="Generic placeholder image">
-              <div class="media-body">
-                  <h5 class="mt-0">Top-aligned media</h5>
-                  <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                  <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+          <div class="row">
+              <div class="col-md-4">
+                  <img
+
               </div>
+              <div class="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
           </div>
 
       </div>
