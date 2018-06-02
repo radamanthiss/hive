cc7b09585c426db68ec6d15433148210596e9516
Kevin Sanchez <kevinsb-93@hotmail.com>
2018-06-02 19:16:03 +0200

    COMMENT 54

--- recomended.php
+++ recomended.php
@@ -6,8 +6,32 @@
  * Time: 4:33 PM
  */
 
-$content = file_get_contents("json/recomended.json");
+$content = file_get_contents("recomended.json");
 $item_data = json_decode($content,true);
 $data = array();
+#var_dump($item_data);
 
-print_r($data);
+$i=0;
+foreach ($item_data['data'] as $key => $item){
+
+    $data[$key]['id'] = $item['id'];
+    $data[$key]['name'] = $item['name'];
+    foreach ($item['recommended'] as $recomended){
+        if($recomended['map']=="SR" && $recomended['mode']=="CLASSIC"){
+            foreach ($recomended['blocks'] as $block){
+                #var_dump($data[$key]['type']);
+                var_dump($block['type']);
+
+            }
+        }
+    }
+
+
+    $i++;
+
+
+
+}
+#var_dump($data);
+
+
